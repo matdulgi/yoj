ff998b6aa40c3761228ef5624499e9bd64f65b9e
unknown <yrdog9494@naver.com>
2021-11-17 22:49:11 +0900

    Initial commit

---
+++ src/main/java/com/yura/yoj/YojApplication.java
@@ -0,0 +1,13 @@
+package com.yura.yoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yura/yoj/YojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yura.yoj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
